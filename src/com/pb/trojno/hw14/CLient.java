ef5e067687dda43af4b797a40fca60e7bcee0a78
Olga Trojno <otronity@gmail.com>
2022-01-16 14:39:11 +0200

    init commit

--- src/com/pb/trojno/hw14/CLient.java
+++ src/com/pb/trojno/hw14/CLient.java
@@ -1,38 +1,79 @@
 package com.pb.trojno.hw14;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+
+
+import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class CLient {
+public class CLient extends Application {
+
     public static void main(String[] args) throws Exception{
+        Application.launch(args);
+    }
+
+    @Override
+    public void start (Stage stage) throws Exception{
+
+        Label label = new Label("Enter message:");
+        TextField msgField = new TextField();
+        TextArea srvMess = new TextArea();
+        Button sendBtn = new Button("send message");
+        Button exitBtn = new Button("EXIT");
+
         String serverIp = "127.0.0.1";
         int serverPort = 9234;
 
         Socket server = new Socket(serverIp, serverPort);
 
-        System.out.println("Клиент стартовал");
         BufferedReader inServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
         PrintWriter outServer = new PrintWriter(server.getOutputStream(), true);
-        BufferedReader inConsole = new BufferedReader(new InputStreamReader(System.in));
 
-        String dataFromUser, dataFromServer;
-
-        // Основной цикл отправки сообщений серверу
-        while ((dataFromUser = inConsole.readLine()) != null) {
-            outServer.println(dataFromUser); //println(dataFromUser);
-            outServer.flush();
-            dataFromServer = inServer.readLine();
-            System.out.println("Сообщение от сервера " + dataFromServer);
-            if ("exit".equalsIgnoreCase(dataFromUser)) {
-                break;
+        sendBtn.setOnAction(e -> {
+            try {
+                String dataFromUser, dataFromServer;
+                dataFromUser = msgField.getText();
+                msgField.clear();
+                outServer.println(dataFromUser);
+                outServer.flush();
+                dataFromServer = inServer.readLine();
+                if (dataFromServer.trim().length() > 0) {
+                    srvMess.appendText(dataFromServer + '\n');
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-        }
-        outServer.close();
-        inServer.close();
-        outServer.close();
-        server.close();
+        });
+
+        exitBtn.setOnAction(e -> {
+            Platform.exit();
+            try {
+                inServer.close();
+                outServer.close();
+                server.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        VBox root = new VBox();
+        root.setSpacing(6);
+        root.getChildren().addAll(label, msgField, srvMess, sendBtn, exitBtn);
+        Scene scene = new Scene(root, 350, 350);
+        stage.setScene(scene);
+        stage.setTitle("Chat Client");
+        stage.show();
 
     }
 }
